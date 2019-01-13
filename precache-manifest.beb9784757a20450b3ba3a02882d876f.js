8cf000f7dce2c21ddf19ea712ce7ab17a3406a6d
ajjubhai <bag3557@gmail.com>
2019-01-13 23:33:38 +0530

    Updates

---
+++ precache-manifest.beb9784757a20450b3ba3a02882d876f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd9c037ecdbefeac5f69698f340e1f47",
+    "url": "/resume_template/static/media/js-square-brands.fd9c037e.svg"
+  },
+  {
+    "revision": "4584195abea4e23443b3",
+    "url": "/resume_template/static/js/main.4584195a.chunk.js"
+  },
+  {
+    "revision": "e36fb90c7c4b8dfb4dc0",
+    "url": "/resume_template/static/js/runtime~main.e36fb90c.js"
+  },
+  {
+    "revision": "b7dd696acdcdffbe0f5c45847fcbbf42",
+    "url": "/resume_template/static/media/html5-brands.b7dd696a.svg"
+  },
+  {
+    "revision": "883d17a5345d48e2ad5c6b55aab3edfa",
+    "url": "/resume_template/static/media/angular-brands.883d17a5.svg"
+  },
+  {
+    "revision": "4c5ee90c264e73f9c0ea6056f9cdf01b",
+    "url": "/resume_template/static/media/css3-alt-brands.4c5ee90c.svg"
+  },
+  {
+    "revision": "a09650e9b65b312492aa3b9416ccf105",
+    "url": "/resume_template/static/media/grunt-brands.a09650e9.svg"
+  },
+  {
+    "revision": "6832fd8da87d3524ebdbe20acb09e884",
+    "url": "/resume_template/static/media/gulp-brands.6832fd8d.svg"
+  },
+  {
+    "revision": "efecbb4f70fe8fa3a71dee49b0a53804",
+    "url": "/resume_template/static/media/wordpress-brands.efecbb4f.svg"
+  },
+  {
+    "revision": "6a39de09da27711af35602c2291e66a1",
+    "url": "/resume_template/static/media/less-brands.6a39de09.svg"
+  },
+  {
+    "revision": "76b3ce1049341f6eba5ae00e5d304de7",
+    "url": "/resume_template/static/media/node-js-brands.76b3ce10.svg"
+  },
+  {
+    "revision": "392e8bc59a429abaa24f8b6d18880689",
+    "url": "/resume_template/static/media/npm-brands.392e8bc5.svg"
+  },
+  {
+    "revision": "c1f4131746d19e4e3bef07114941b5cf",
+    "url": "/resume_template/static/media/react-brands.c1f41317.svg"
+  },
+  {
+    "revision": "a6e6acbce0390c096f15bbfe0e9d1567",
+    "url": "/resume_template/static/media/sass-brands.a6e6acbc.svg"
+  },
+  {
+    "revision": "94c1c869bb21dd6364be",
+    "url": "/resume_template/static/js/1.94c1c869.chunk.js"
+  },
+  {
+    "revision": "21e3111cbcf367d4467fd0d49aa1855e",
+    "url": "/resume_template/index.html"
+  }
+];
